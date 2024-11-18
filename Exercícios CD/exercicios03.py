5455d456d55461d0ec7d46ac6b4cba08787a373a
Liz Martins <martins.flordeliz@gmail.com>
2024-11-18 17:03:01 -0300

    exercícios

--- Exercícios CD/exercicios03.py
+++ Exercícios CD/exercicios03.py
@@ -1,1 +1,10 @@
 """Crie um programa que leia o peso de uma carga em números inteiros. Se o peso for até 10kg, informe que o valor será de R$50,00. entre 11 e 20kg, informe que o valor será de R$ 80. Se for maior que 20 informe que o transporte não é aceito. teste vários pesos"""
+
+peso = int(input("Insira o valor da carga: "))
+
+if peso <=10 and peso >0:
+    print("Valor R$50,00")
+elif peso >=11 and peso <=20 :
+    print("Valor R$80,00")
+else :
+    print("Transporte não é aceito")
